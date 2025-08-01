ccb7f0649b6fe827ac5c5e00728a885a3b261634
garrett391 <garrett.michael@outlook.com>
2025-07-31 22:14:14 -0700

    starts tracking user measurements

---
+++ Recipes.accdb.src/tbldefs/MeasurementLocations.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [MeasurementLocations] (
+  [MeasurementLocationID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [MeasurementLocation] VARCHAR (255),
+  [Instruction] VARCHAR (255)
+)
---
+++ Recipes.accdb.src/tbldefs/UserMeasurements.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [UserMeasurements] (
+  [MeasurementID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [UserID] LONG CONSTRAINT [UsersUserMeasurements] REFERENCES [Users] ([UserID]),
+  [MeasurementDate] DATETIME,
+  [MeasurementLocationID] LONG CONSTRAINT [MeasurementLocationsUserMeasurements] REFERENCES [MeasurementLocations] ([MeasurementLocationID]),
+  [Size] VARCHAR (255)
+)
